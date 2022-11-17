bd4e8d7d81cb23851347ad2a18642e6965a77c4d
Jan Andrle <andrle.jan@centrum.cz>
2022-11-17 18:32:32 +0100

    :tada:

---
+++ main.js
@@ -0,0 +1,14 @@
+/* jshint esversion: 6,-W097, -W040, browser: true, expr: true, undef: true */
+/* global prompt */
+console.log("{{site.data.social.account_mastodon.account}}");
+
+window.mastodonShare= function(event){
+	event.preventDefault();
+	const { target }= event;
+	const href= target.getAttribute("data-href");
+	const domain= prompt("Zadejte svoji Mastodon instanci:", "mastodon.social");
+	if(!domain) return;
+	location.href= "https://"+domain+href;
+};
+
+document.documentElement.classList.remove("nojs");
